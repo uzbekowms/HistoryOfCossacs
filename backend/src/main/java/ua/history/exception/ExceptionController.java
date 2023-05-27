87c1d5fb1c0b86d486e2e91d20ccd20861d18bd9
uzbek <lukasowms@gmail.com>
2023-05-27 23:32:07 +0300

    Rename ExceptionHandlerController -> ExceptionController

--- backend/src/main/java/ua/history/exception/ExceptionController.java
+++ backend/src/main/java/ua/history/exception/ExceptionController.java
@@ -3,13 +3,12 @@ package ua.history.exception;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class ExceptionHandlerController {
+public class ExceptionController {
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
