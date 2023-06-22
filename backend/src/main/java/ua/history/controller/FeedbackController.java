26653e374d0066d71d39f969c241528ac45789d0
uzbekowms <lukasowms@gmail.com>
2023-06-22 14:16:31 +0300

    Add feedback posting

--- backend/src/main/java/ua/history/controller/FeedbackController.java
+++ backend/src/main/java/ua/history/controller/FeedbackController.java
@@ -3,8 +3,8 @@ package ua.history.controller;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ua.history.dto.UserFeedbackDTO;
 import ua.history.model.UserFeedback;
 import ua.history.service.UserFeedbackService;
 
@@ -17,7 +17,12 @@ public class FeedbackController {
 
     private final UserFeedbackService feedbackService;
 
+    @GetMapping
     public ResponseEntity<List<UserFeedback>> getFeedbacks(){
         return ResponseEntity.ok(feedbackService.findAll());
+    }
+    @PostMapping
+    public ResponseEntity<UserFeedback> addFeedback(@RequestBody UserFeedbackDTO feedback){
+        return ResponseEntity.ok(feedbackService.save(feedback));
     }
 }
